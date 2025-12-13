4d24288c7aa806df056fefc3a547a2efb743c1d8
4090rt <artem2007yannurow@gmail.com>
2025-12-13 20:34:34 +0500

    Add files via upload

---
+++ Main/TelegramBotService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums; 
+
+
+namespace TelegramConvertorBots.Main
+{
+    public class TelegramBotService : IHostedService
+    {        
+        private readonly ITelegramBotClient _botClient;
+        private readonly ILogger<TelegramBotService> _logger;
+        private readonly Models.BotConfig _config;
+        private readonly CommandHandler.CommandHandlerr _commandHandler;
+    }
+}
