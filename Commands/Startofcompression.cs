28c961948df8e32dd42ed48c73d25a9add1c5ae1
4090rt <artem2007yannurow@gmail.com>
2026-01-04 18:47:13 +0500

    Add files via upload

---
+++ Commands/Startofcompression.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+
+namespace TelegramConvertorBots.Commands
+{
+    public class Startofcompression
+    {
+        private readonly ITelegramBotClient _botClient;
+        public Startofcompression(ITelegramBotClient botclient)
+        { 
+            _botClient = botclient;
+        }
+
+        public async Task SendStartofcompression(long chatId, CancellationToken cancellationToken)
+        {
+            string text = "Вы можете уменьшить размер изображения, просто отправьте изображение в чат";
+
+            await _botClient.SendTextMessageAsync(
+                    chatId: chatId,
+                    text: text,
+                    parseMode: ParseMode.Html,
+                    cancellationToken: cancellationToken);
+        }
+    }
+}
