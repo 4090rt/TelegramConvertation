1cc398e96e91e0811ed0d3abd769f4c4cf2b2065
4090rt <artem2007yannurow@gmail.com>
2026-02-09 22:56:13 +0500

    Add files via upload

---
+++ HttpBlock/Model.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramConvertorBots.HttpBlock
+{
+    public class Model
+    {
+            public string city { get; set; }
+
+            public string region { get; set; }
+
+            public string country { get; set; }
+
+            public string org { get; set; }
+
+            public string postal { get; set; }
+
+            public string timezone { get; set; }
+    }
+}
---
+++ HttpBlock/ParsedClass.cs
@@ -0,0 +1,40 @@
+﻿using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramConvertorBots.HttpBlock
+{
+    public class ParsedClass
+    {
+        private readonly Microsoft.Extensions.Logging.ILogger<ParsedClass> _logger;
+
+        public ParsedClass(Microsoft.Extensions.Logging.ILogger<ParsedClass> logger)
+        { 
+            _logger = logger;
+        }
+
+        public async Task<Model> Parse(Stream stream)
+        {
+            try
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var result = await reader.ReadToEndAsync();
+
+                    var options = new JsonSerializeOptions
+                    {
+                        
+                    };
+                }
+            }
+            catch (Exception ex)
+            { 
+            
+            }
+        }
+    }
+}
